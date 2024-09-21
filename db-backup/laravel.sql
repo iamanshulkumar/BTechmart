eaca90dc91fcfb16290c3765ac4032e8ff88df43
Anshul Kumar <anshulkm17@gmail.com>
2024-09-21 17:58:51 +0530

    updated db

--- db-backup/laravel.sql
+++ db-backup/laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2024 at 09:20 AM
+-- Generation Time: Sep 21, 2024 at 01:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -112,6 +112,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('b3bCc5kL1DGT1kfMNhH5ezKIzliy349e1MpzK7SZ', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiTWtkZEJUTmxLdmc0c1dwYTdsbWc5eDZkdzZQZWswOFlVSkpZTlpYcSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC92aWV3bWFzdGVyIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czozOiJ1cmwiO2E6MDp7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7czoyMToicGFzc3dvcmRfaGFzaF9zYW5jdHVtIjtzOjYwOiIkMnkkMTAkU0daa0lDVHZ2LnFQOWg5amtPVHF4LjRqb1BSZ2IxNmc4a3VvNGFCcUhLTXVFR0U2ejEvdi4iO30=', 1726917469),
 ('MZe6nCXrX1IEbANqFYWohZc6dLhJNoGc7N86opuj', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoieUpiSUlkTWxab05vT3ZpVU5jYVFoa3o4ZTAxTkFXUHZTTDZhMWZpOCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9BZG1pbi9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJFNHWmtJQ1R2di5xUDloOWprT1RxeC40am9QUmdiMTZnOGt1bzRhQnFIS011RUdFNnoxL3YuIjt9', 1712127804);
 
 -- --------------------------------------------------------
@@ -141,7 +142,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `two_factor_confirmed_at`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
-(1, 'Noizy', 'nb@gmail.com', NULL, '$2y$10$SGZkICTvv.qP9h9jkOTqx.4joPRgb16g8kuo4aBqHKMuEGE6z1/v.', NULL, NULL, NULL, NULL, NULL, NULL, '2024-04-01 00:15:00', '2024-04-01 00:15:00');
+(1, 'Admin', 'admin@btechmart.com', NULL, '$2y$10$SGZkICTvv.qP9h9jkOTqx.4joPRgb16g8kuo4aBqHKMuEGE6z1/v.', NULL, NULL, NULL, NULL, NULL, NULL, '2024-04-01 00:15:00', '2024-04-01 00:15:00');
 
 --
 -- Indexes for dumped tables
